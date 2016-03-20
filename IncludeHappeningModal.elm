1829687fd7edb208d3d6057fd38711a485daf0cc
Igor Cananea <icc@avalonbits.com>
2016-03-20 15:06:59 -0300

    Modal now has a text input and buttons.

--- IncludeHappeningModal.elm
+++ IncludeHappeningModal.elm
@@ -1,5 +1,6 @@
 module IncludeHappeningModal where
 
+import Html as H
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
@@ -14,7 +15,7 @@ type alias Model =
 
 init : Model
 init =
-    { modalTitle = "What happened?"
+    { modalTitle = "It happened"
     , title = ""
     , description = ""
     }
@@ -25,7 +26,22 @@ init =
 view : Signal.Address () -> Model -> Html
 view x model =
     div [ id "whatHappened", class "modal modal-fixed-footer" ]
-    [ div [ class "modal-content" ]
-          [ h4 [] [ text model.modalTitle ] ],
-      div [ class "modal-footer" ] [ text "Omega" ]
-    ]
+        [ div [ class "modal-content" ]
+              [ h4 [] [ text model.modalTitle ]
+              , H.form [ class "col s12" ]
+                     [ div [ class "row"  ]
+                           [ div [ class "input-field col s12"]
+                                  [ input [ placeholder "What happened?"
+                                          , id "what_happened"
+                                          ] []
+                                  ]
+                           ]
+                    ]
+             ]
+        , div [ class "modal-footer" ]
+              [ a [ href "#!", class "modal-action modal-close waves-effect waves-red btn-flat" ]
+                  [ text "Cancel" ]
+              , a [ href "#!", class "modal-action modal-close waves-effect waves-red btn-flat" ]
+                  [ text "OK" ]
+              ]
+        ]
